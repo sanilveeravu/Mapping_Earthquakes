eb5978e5ce32868d40450120e603858a0eff19e7
sanilveeravu <mailsanilnow@yahoo.com>
2022-06-15 21:42:22 -0500

    layered

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -5,7 +5,42 @@ console.log("working");
 //let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 //let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 //let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+//let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+//let map = L.map('mapid').setView([30, 30], 2);
+
+
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Accessing the airport GeoJSON URL
+let airportData = 'https://raw.githubusercontent.com/sanilveeravu/Mapping_Earthquakes/main/majorAirports.json'
+
+
+
 
 
 // Grabbing our GeoJSON data.
@@ -18,13 +53,13 @@ let map = L.map('mapid').setView([37.5, -122.5], 10);
 //   }
 // }).addTo(map);
 
-L.geoJSON(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup(`<h3>Airport code: ${feature.properties.faa}</h3><hr><h3>Airport Name: ${feature.properties.name}</h3>`);
-    }
-  }).addTo(map);
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     onEachFeature: function(feature, layer) {
+//       console.log(layer);
+//       layer.bindPopup(`<h3>Airport code: ${feature.properties.faa}</h3><hr><h3>Airport Name: ${feature.properties.name}</h3>`);
+//     }
+//   }).addTo(map);
 
 // Coordinates for each point to be used in the line.
 // let line = [
@@ -109,15 +144,42 @@ L.geoJSON(sanFranAirport, {
 
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//navigation-night-v1
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    //id: 'mapbox/streets-v11',
-    //tileSize: 512,
-    //zoomOffset: -1,
-    accessToken: API_KEY
-});
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     //id: 'mapbox/streets-v11',
+//     //tileSize: 512,
+//     //zoomOffset: -1,
+//     accessToken: API_KEY
+// });
+
+
+
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);     
+//streets.addTo(map);     
+
+L.control.layers(baseMaps).addTo(map);
+
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data){
+  console.log(data);
+   // Creating a GeoJSON layer with the retrieved data.
+   L.geoJSON(data, {
+        // We turn each feature into a marker on the map.
+        onEachFeature: function(feature, layer) {
+          console.log(layer);
+          layer.bindPopup(`<h3>Airport code: ${feature.properties.faa}</h3><hr><h3>Airport Name: ${feature.properties.name}</h3>`);
+        }
+      }).addTo(map);
+});
+
+
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     onEachFeature: function(feature, layer) {
+//       console.log(layer);
+//       layer.bindPopup(`<h3>Airport code: ${feature.properties.faa}</h3><hr><h3>Airport Name: ${feature.properties.name}</h3>`);
+//     }
+//   }).addTo(map);
